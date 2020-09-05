8f02cafa9102e52dab768975b5bf117c157ed009
unknown <sansaha0505@gmail.com>
2020-09-05 14:52:39 +0530

    Updates

---
+++ precache-manifest.7398e24c8605fbcfa0b63cb025204767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f4a1066fac388e9e92b875b5d846b8",
+    "url": "/keep/index.html"
+  },
+  {
+    "revision": "a986315cb523554ef329",
+    "url": "/keep/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "10b2bebf538a6524b1bf",
+    "url": "/keep/static/css/main.e34c1b5a.chunk.css"
+  },
+  {
+    "revision": "a986315cb523554ef329",
+    "url": "/keep/static/js/2.ed9d9f30.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keep/static/js/2.ed9d9f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b2bebf538a6524b1bf",
+    "url": "/keep/static/js/main.10df2485.chunk.js"
+  },
+  {
+    "revision": "f7f3d2a65846bc2f99dd",
+    "url": "/keep/static/js/runtime-main.55bce19b.js"
+  }
+]);
